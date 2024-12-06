d94218986bec52dfb725dd9c0ab60123cc6547eb
Teddy Wilson <hello@teddywilson.me>
2024-12-05 23:12:12 -0500

    enable cron

--- sync.sh
+++ sync.sh
@@ -2,23 +2,18 @@
 #
 # Downloads data from multiple sheets in a Google Sheets document and stores them as separate CSV files.
 
-# Google Sheets document ID.
 DOCUMENT_ID="2PACX-1vRO4CR0Z9OLYQYTdIP-lxbWXFPex26Iaelm0cElTBLQidovERlAeXfdzbjWrom5DEr3Do5Z7ZOwKjKH"
 
-# The current directory of this script, regardless of where it's executed from.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Destination directory where resulting CSVs will be stored. Create it if it doesn't already exist.
 DIR_DST="${SCRIPT_DIR}/src/data"
 mkdir -p "${DIR_DST}"
 
-# Sheet details (name and gid)
 SHEETS=(
   "calendar:0"
   "text:407807123"
 )
 
-# Loop through each sheet and download its data as a CSV.
 for SHEET in "${SHEETS[@]}"; do
   NAME=$(echo "$SHEET" | cut -d':' -f1)
   GID=$(echo "$SHEET" | cut -d':' -f2)
